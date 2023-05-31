242ffba01f1d963112910a24d28879ec88c91a08
Gabriel Gregório <gabriel.gregorio@pravaler.com.br>
2023-05-31 15:46:51 -0300

    fix: update layout

--- src/baseLayout.tsx
+++ src/baseLayout.tsx
@@ -2,12 +2,7 @@ import { ReactNode } from "react";
 
 export const BaseLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div
-      style={{
-        maxHeight: "calc(100vh - 64px)",
-      }}
-      className="bg-[#FBFBFB] mt-[64px] scroll-smooth overflow-hidden overflow-y-scroll scrollbar animate-fadeIn"
-    >
+    <div className="bg-[#FBFBFB] scroll-smooth overflow-hidden overflow-y-scroll scrollbar animate-fadeIn">
       {children}
     </div>
   );
