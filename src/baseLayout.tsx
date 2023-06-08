cb6d5c2ac42b960536cb6b5bb25615ccf41241af
Gabriel Gregório <gabriel.gregorio@pravaler.com.br>
2023-06-08 16:09:09 -0300

    fefactor: rename gallery

--- src/baseLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export const BaseLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className="bg-[#FBFBFB] scroll-smooth overflow-hidden overflow-y-scroll scrollbar animate-fadeIn">
-      {children}
-    </div>
-  );
-};
